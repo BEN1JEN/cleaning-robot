2651d98843e5ccf21bbe25a78788a924ba501d7a
BEN1JEN <benpheard@gmail.com>
2022-05-15 13:31:57 -0600

    Fix

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use gpio::sysfs::{SysFsGpioInput, SysFsGpioOutput};
 use gpio::{GpioIn, GpioOut};
-use std::{thread, time};
 
-const IR_LEFT_PIN: u32 = 4;
-const IR_RIGHT_PIN: u32 = 17;
-const DIST_FRONT_TRIGGER_PIN: u32 = 27;
-const DIST_FRONT_ECHO_PIN: u32 = 22;
-const MOTOR_LEFT_EN_PIN: u32 = 14;
-const MOTOR_LEFT_IN0_PIN: u32 = 15;
-const MOTOR_LEFT_IN1_PIN: u32 = 18;
-const MOTOR_RIGHT_EN_PIN: u32 = 10;
-const MOTOR_RIGHT_IN0_PIN: u32 = 9;
-const MOTOR_RIGHT_IN1_PIN: u32 = 11;
+use std::time::Instant;
+
+const IR_LEFT_PIN: u16 = 4;
+const IR_RIGHT_PIN: u16 = 17;
+const DIST_FRONT_TRIGGER_PIN: u16 = 27;
+const DIST_FRONT_ECHO_PIN: u16 = 22;
+const MOTOR_LEFT_EN_PIN: u16 = 14;
+const MOTOR_LEFT_IN0_PIN: u16 = 15;
+const MOTOR_LEFT_IN1_PIN: u16 = 18;
+const MOTOR_RIGHT_EN_PIN: u16 = 10;
+const MOTOR_RIGHT_IN0_PIN: u16 = 9;
+const MOTOR_RIGHT_IN1_PIN: u16 = 11;
 
 struct Motor {
 	en: SysFsGpioOutput,
@@ -19,12 +20,12 @@ struct Motor {
 	in1: SysFsGpioOutput,
 	speed: f32,
 	freq: f32,
-	on_time: f32,
+	timer: f32,
 	on: bool,
 }
 
 impl Motor {
-	fn new(en_pin: u32, in0_pin: u32, in1_pin: u32) -> Motor {
+	fn new(en_pin: u16, in0_pin: u16, in1_pin: u16) -> Motor {
 		Motor {
 			en: SysFsGpioOutput::open(en_pin).unwrap(),
 			in0: SysFsGpioOutput::open(in0_pin).unwrap(),
@@ -39,16 +40,16 @@ impl Motor {
 		if let Some(speed) = speed {
 			let speed = speed.clamp(-1.0, 1.0);
 			let abs_speed = speed.abs();
-			self.en.set_high();
+			self.en.set_high().unwrap();
 			if abs_speed <= 0.0001 {
-				self.in0.set_low();
-				self.in1.set_low();
-			} else if speed > 0 {
-				self.in0.set_low();
-				self.in1.set_high();
+				self.in0.set_low().unwrap();
+				self.in1.set_low().unwrap();
+			} else if speed > 0.0 {
+				self.in0.set_low().unwrap();
+				self.in1.set_high().unwrap();
 			} else {
-				self.in0.set_high();
-				self.in1.set_low();
+				self.in0.set_high().unwrap();
+				self.in1.set_low().unwrap();
 			}
 		}
 	}
@@ -57,15 +58,13 @@ impl Motor {
 		if self.on {
 			if self.timer >= self.speed / self.freq {
 				self.on = false;
-				self.en.set_low();
+				self.en.set_low().unwrap();
 				self.timer = 0.0;
 			}
-		} else {
-			if self.timer >= (1.0 - self.speed) / self.freq {
-				self.on = true;
-				self.en.set_high();
-				self.timer = 0.0;
-			}
+		} else if self.timer >= (1.0 - self.speed) / self.freq {
+			self.on = true;
+			self.en.set_high().unwrap();
+			self.timer = 0.0;
 		}
 	}
 }
@@ -76,15 +75,15 @@ struct Drive {
 }
 
 impl Drive {
-	fn new(left_en_pin: u32, left_in0_pin: u32, left_in1_pin: u32) -> Drive {
+	fn new(left_en_pin: u16, left_in0_pin: u16, left_in1_pin: u16, right_en_pin: u16, right_in0_pin: u16, right_in1_pin: u16) -> Drive {
 		Drive {
-			left: Motor::new(left_en_pin: u32, left_in0_pin: u32, left_in1_pin: u32),
-			right: Motor::new(right_en_pin: u32, right_in0_pin: u32, right_in1_pin: u32),
+			left: Motor::new(left_en_pin, left_in0_pin, left_in1_pin),
+			right: Motor::new(right_en_pin, right_in0_pin, right_in1_pin),
 		}
 	}
 	fn set_drive(&mut self, speed: f32, turn: f32) {
-		self.left.set_speed(speed + turn);
-		self.right.set_speed(speed - turn);
+		self.left.set_speed(Some(speed + turn));
+		self.right.set_speed(Some(speed - turn));
 	}
 	fn update(&mut self, delta_time: f32) {
 		self.left.update_pwm(delta_time);
@@ -97,16 +96,16 @@ fn main() {
 		MOTOR_LEFT_EN_PIN,
 		MOTOR_LEFT_IN0_PIN,
 		MOTOR_LEFT_IN1_PIN,
-		MOTOR_LEFT_EN_PIN,
-		MOTOR_LEFT_IN0_PIN,
-		MOTOR_LEFT_IN1_PIN,
+		MOTOR_RIGHT_EN_PIN,
+		MOTOR_RIGHT_IN0_PIN,
+		MOTOR_RIGHT_IN1_PIN,
 	);
 	let mut last_time = Instant::now();
 
 	drive.set_drive(0.0, 1.0);
 	loop {
 		let time = Instant::now();
-		let delta_time = time.duration_since(last_time).as_secs();
+		let delta_time = time.duration_since(last_time).as_secs_f32();
 		last_time = time;
 
 		drive.update(delta_time);
